6d786917e9d469ae9971de3c6f0522ab9dba614a
Pumipuch <jokepmpp@gmail.com>
2024-04-26 16:05:15 +0700

    update middleware

--- middleware.js
+++ middleware.js
@@ -2,7 +2,8 @@ import { NextResponse } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-  return NextResponse.redirect(new URL('/', request.url))
+//   return NextResponse.redirect(new URL('/home', request.url))
+    return NextResponse.next()
 }
  
 // See "Matching Paths" below to learn more
